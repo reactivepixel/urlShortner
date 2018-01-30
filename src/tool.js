7a4e1b402e00da2ed978b115a78392c394351baf
Justin Weber <jjweber@fullsail.edu>
2018-01-30 18:13:56 -0500

    Changed genLoopIndex
    Original line:
    for(var genLoopIndex = 0; genLoopIndex < 50; genLoopIndex++)
    Changed to:
    for(var genLoopIndex = 0; genLoopIndex < 150; genLoopIndex++)

--- src/tool.js
+++ src/tool.js
@@ -2,11 +2,12 @@ exports.genURL = (urlLength) => {
   var urlString = "";
   const sourceLetters = "ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";
 
-  for(var genLoopIndex = 0; genLoopIndex < 50; genLoopIndex++){
+  for(var genLoopIndex = 0; genLoopIndex < 150; genLoopIndex++){
 
     // Add a random letter to the urlString
     urlString += sourceLetters.charAt(Math.random() * (sourceLetters.length - 1))
   }
 
   return urlString
+  
 }
