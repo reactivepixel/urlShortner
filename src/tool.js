d4c9f72d46f83bb737c042ed63ddcf0004837c26
Michael  Sewell <MikeSewell01@gmail.com>
2018-02-27 18:29:21 -0500

    fixed broken code

--- src/tool.js
+++ src/tool.js
@@ -2,11 +2,11 @@ exports.genURL = (urlLength) => {
   var urlString = "";
   const sourceLetters = "ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";
 
-  for(var genLoopIndex = 0; genLoopIndex < 50; genLoopIndex++){
+  for (var genLoopIndex = 0; genLoopIndex < urlLength; genLoopIndex++) {
 
     // Add a random letter to the urlString
     urlString += sourceLetters.charAt(Math.random() * (sourceLetters.length - 1))
   }
-
+  console.log(sourceLetters.length);
   return urlString
 }
